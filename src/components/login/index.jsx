f02961f46fbe12efa165a4d93c682e791b20cd8f
suyanqa <suyan1254088@gmail.com>
2024-04-07 12:13:39 +0800

    suyanqa
    用户已进入网页默认的配置首页就展示登录组件

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import './index.css';
 import Menu from '../Menu';
 
-class Index extends Component {
+class Index extends Component { 
   constructor(props) {
     super(props);
     // 设置初始化状态
@@ -51,66 +51,66 @@ class Index extends Component {
 
   render() {
     const { isLoggedIn, message } = this.state;
-
+  
     return (
-      <div className="login-container">
-        <div className="login-box">
-          <h3>博速汽修厂</h3>
-          <hr />
-          {isLoggedIn ? ( // 根据登录状态决定是否渲染菜单组件
-            <Menu />
-          ) : (
-            <div>
-              <Form
-                name="normal_login"
-                className="login-form"
-                labelCol={{ span: 4 }}
-                wrapperCol={{ span: 14 }}
-                initialValues={{ remember: true }}
-                onFinish={this.onFinish}
-              >
-                <Form.Item
-                  name="username"
-                  rules={[{ required: true, message: '请输入你的用户名!' }]}
-                  className="usernames"
+      <div>
+        {isLoggedIn ? (
+          <Menu />
+        ) : (
+          <div>
+            <div className="login-container">
+              <div className="login-box">
+                <h3>博速汽修厂</h3>
+                <hr />
+                <Form
+                  name="normal_login"
+                  className="login-form"
+                  labelCol={{ span: 4 }}
+                  wrapperCol={{ span: 14 }}
+                  initialValues={{ remember: true }}
+                  onFinish={this.onFinish}
                 >
-                  <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="请输入你的用户名" />
-                </Form.Item>
-                <Form.Item
-                  name="password"
-                  rules={[{ required: true, message: '请输入你的密码!' }]}
-                >
-                  <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="请输入你的密码" />
-                </Form.Item>
-                <Form.Item>
-                  <Form.Item name="remember" valuePropName="checked" noStyle>
-                    <Checkbox>记住我</Checkbox>
+                  <Form.Item
+                    name="username"
+                    rules={[{ required: true, message: '请输入你的用户名!' }]}
+                    className="usernames"
+                  >
+                    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="请输入你的用户名" />
                   </Form.Item>
-                  <a className="login-form-forgot" href="http://127.0.0.1:3001">
-                    忘记密码
-                  </a>
-                </Form.Item>
-                <Form.Item>
-                  <Button type="primary" htmlType="submit" className="login-form-button">
-                    登录
-                  </Button>
-                  {/*&nbsp;&nbsp;<a href="http://127.0.0.1:3000">现在注册</a>**/}
-                </Form.Item>
-                {/* 
-                  判断用户是否登录然后给出提示信息
-              **/}
-                {isLoggedIn ? (
-                  <Alert message="Error" type="error" showIcon />
-                ) : (
-                  <p>{message}</p>
-                )}
-              </Form>
+                  <Form.Item
+                    name="password"
+                    rules={[{ required: true, message: '请输入你的密码!' }]}
+                  >
+                    <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="请输入你的密码" />
+                  </Form.Item>
+                  <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                      <Checkbox>记住我</Checkbox>
+                    </Form.Item>
+                    <a className="login-form-forgot" href="http://127.0.0.1:3001">
+                      忘记密码
+                    </a>
+                  </Form.Item>
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                      登录
+                    </Button>
+                  </Form.Item>
+                  {/* 判断用户是否登录然后给出提示信息 */}
+                  {isLoggedIn ? (
+                    <Alert message="Error" type="error" showIcon />
+                  ) : (
+                    <p>{message}</p>
+                  )}
+                </Form>
+              </div>
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     );
   }
+  
 }
 
 export default Index;
